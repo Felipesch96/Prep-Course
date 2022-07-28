6dba8f7714114cfca4c75d22cddf48cfd540a368
unknown <felipe.schwindt36@gmail.com>
2022-07-28 12:04:31 -0300

    final commit HW JSI

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -288,30 +288,14 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  var longitud = letra.lenght;
-  if (longitud == 1) {
-    if (letra == 'a') {
-      return "Es vocal";
-  }
-    else if (letra == 'e'){
-      return "Es vocal"
-    }
-    else if (letra == 'i'){
-      return "Es vocal"
-    }
-    else if (letra == 'o'){
-      return "Es vocal"
-    }
-    else if (letra == 'u'){
-      return "Es vocal"
-    }
-    else {
-      return "Dato incorrecto"
-    }
-  }
-  else {
+  if(letra.length > 1){
     return "Dato incorrecto"
   }
+  if(letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u"){
+    return "Es vocal"
+  }
+  return "Dato incorrecto"
+
 }
 
 
